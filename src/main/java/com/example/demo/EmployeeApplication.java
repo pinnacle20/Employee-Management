6a6b61c8682d948f2efb01d91b0d77eca069f10d
pinnacle20 <73813570+pinnacle20@users.noreply.github.com>
2023-09-10 20:32:50 +0530

    Backend Setup Done

--- src/main/java/com/example/demo/EmployeeApplication.java
+++ src/main/java/com/example/demo/EmployeeApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployApplication {
+public class EmployeeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployApplication.class, args);
+		SpringApplication.run(EmployeeApplication.class, args);
 	}
-
 }
