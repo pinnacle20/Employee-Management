90298dbfee23694fdbd2a38352ae24928cbb3f11
pinnacle20 <73813570+pinnacle20@users.noreply.github.com>
2023-09-13 01:29:00 +0530

    add employee api done

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,15 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    // Get employees list 
     @GetMapping("/employees")
     public List<Employee> getAllEmployee(){
         return employeeRepository.findAll();
+    }
+
+    // Create employees 
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
     }
 }
